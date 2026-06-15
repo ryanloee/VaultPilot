d2e451808eece25430be5ae7d7773913962de7e9
ryanloee <1263094915@qq.com>
2026-06-16 03:57:46 +0800

    style: cargo fmt

--- src/bin/vaultpilot-cli.rs
+++ src/bin/vaultpilot-cli.rs
@@ -975,7 +975,10 @@ async fn run_http_bridge(
             rate_limit_middleware,
         ))
         .layer(DefaultBodyLimit::max(10 * 1024 * 1024)) // 10 MB
-        .layer(TimeoutLayer::with_status_code(StatusCode::GATEWAY_TIMEOUT, std::time::Duration::from_secs(180))) // #605: overall request timeout
+        .layer(TimeoutLayer::with_status_code(
+            StatusCode::GATEWAY_TIMEOUT,
+            std::time::Duration::from_secs(180),
+        )) // #605: overall request timeout
         .layer(
             CorsLayer::new()
                 .allow_origin(AllowOrigin::predicate(|origin, _parts| {

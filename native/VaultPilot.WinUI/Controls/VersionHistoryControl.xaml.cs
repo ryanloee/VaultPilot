e389a746288128474e4c0ebe8a130988c3d89b46
ryanloee <1263094915@qq.com>
2026-07-24 11:07:16 +0800

    fix: add missing using Windows.UI.Text for FontWeight in VersionHistoryControl

--- native/VaultPilot.WinUI/Controls/VersionHistoryControl.xaml.cs
+++ native/VaultPilot.WinUI/Controls/VersionHistoryControl.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Text;
+using Windows.UI.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
